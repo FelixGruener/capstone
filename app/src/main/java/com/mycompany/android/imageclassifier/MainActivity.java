470249e3dd79c68cdb6894e920707638c789a8ad
Delaroy Studios <bamoyk@yahoo.com>
2018-08-24 14:11:54 +0100

    setting up the image selection

--- app/src/main/java/com/mycompany/android/imageclassifier/MainActivity.java
+++ app/src/main/java/com/mycompany/android/imageclassifier/MainActivity.java
@@ -1,13 +1,303 @@
 package com.mycompany.android.imageclassifier;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    @BindView(R.id.selectImage)
+    ImageView selectImage;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recycler_view;
+
+    @BindView(R.id.image_header)
+    ImageView image_header;
+
+    private static final int REQUEST_TAKE_PHOTO = 0;
+    private static final int REQUEST_PICK_PHOTO = 2;
+    private Uri mMediaUri;
+    private static final int CAMERA_PIC_REQUEST = 1111;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private Uri fileUri;
+    private String mediaPath;
+    private String mImageFileLocation = "";
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+    private String postPath;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        selectImage.setOnClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            selectImage.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        } else {
+            selectImage.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectImage.setEnabled(true);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.selectImage:
+                new MaterialDialog.Builder(this)
+                        .title(R.string.uploadImages)
+                        .items(R.array.uploadImages)
+                        .itemsIds(R.array.itemIds)
+                        .itemsCallback(new MaterialDialog.ListCallback() {
+                            @Override
+                            public void onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                                switch (which) {
+                                    case 0:
+                                        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                        startActivityForResult(galleryIntent, REQUEST_PICK_PHOTO);
+                                        break;
+                                    case 1:
+                                        captureImage();
+                                        break;
+                                    case 2:
+                                        image_header.setImageResource(R.color.colorPrimary);
+                                        break;
+                                }
+                            }
+                        })
+                        .show();
+                break;
+        }
+
+    }
+
+    /**
+     * Launching camera app to capture image
+     */
+    private void captureImage() {
+        if (Build.VERSION.SDK_INT > 21) { //use this if Lollipop_Mr1 (API 22) or above
+            Intent callCameraApplicationIntent = new Intent();
+            callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // We give some instruction to the intent to save the image
+            File photoFile = null;
+
+            try {
+                // If the createImageFile will be successful, the photo file will have the address of the file
+                photoFile = createImageFile();
+                // Here we call the function that will try to catch the exception made by the throw function
+            } catch (IOException e) {
+                Logger.getAnonymousLogger().info("Exception error in generating the file");
+                e.printStackTrace();
+            }
+            // Here we add an extra file to the intent to put the address on to. For this purpose we use the FileProvider, declared in the AndroidManifest.
+            Uri outputUri = FileProvider.getUriForFile(
+                    this,
+                    BuildConfig.APPLICATION_ID + ".provider",
+                    photoFile);
+            callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+
+            // The following is a new line with a trying attempt
+            callCameraApplicationIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+            Logger.getAnonymousLogger().info("Calling the camera App by intent");
+
+            // The following strings calls the camera app and wait for his file in return.
+            startActivityForResult(callCameraApplicationIntent, CAMERA_PIC_REQUEST);
+        } else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            // start the image capture Intent
+            startActivityForResult(intent, CAMERA_PIC_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_TAKE_PHOTO || requestCode == REQUEST_PICK_PHOTO) {
+                if (data != null) {
+                    // Get the Image from data
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    assert cursor != null;
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    mediaPath = cursor.getString(columnIndex);
+                    // Set the Image in ImageView for Previewing the Media
+                    image_header.setImageBitmap(BitmapFactory.decodeFile(mediaPath));
+                    cursor.close();
+
+                    postPath = mediaPath;
+                }
+
+
+            }else if (requestCode == CAMERA_PIC_REQUEST){
+                if (Build.VERSION.SDK_INT > 21) {
+
+                    Glide.with(this).load(mImageFileLocation).into(image_header);
+                    postPath = mImageFileLocation;
+
+                }else{
+                    Glide.with(this).load(fileUri).into(image_header);
+                    postPath = fileUri.getPath();
+                }
+            }
+        }
+        else if (resultCode != RESULT_CANCELED) {
+            Toast.makeText(this, "Sorry, there was an error!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    File createImageFile() throws IOException {
+        Logger.getAnonymousLogger().info("Generating the image - method started");
+
+        // Here we create a "non-collision file name", alternatively said, "an unique filename" using the "timeStamp" functionality
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmSS").format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp;
+        // Here we specify the environment location and the exact path where we want to save the so-created file
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/photo_saving_app");
+        Logger.getAnonymousLogger().info("Storage directory set");
+
+        // Then we create the storage directory if does not exists
+        if (!storageDirectory.exists()) storageDirectory.mkdir();
+
+        // Here we create the file using a prefix, a suffix and a directory
+        File image = new File(storageDirectory, imageFileName + ".jpg");
+        // File image = File.createTempFile(imageFileName, ".jpg", storageDirectory);
+
+        // Here the location is saved into the string mImageFileLocation
+        Logger.getAnonymousLogger().info("File name and path set");
+
+        mImageFileLocation = image.getAbsolutePath();
+        fileUri = Uri.parse(mImageFileLocation);
+        // The file is returned to the previous intent across the camera application
+        return image;
+    }
+
+    /**
+     * Here we store the file url as it will be null after returning from camera
+     * app
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save file url in bundle as it will be null on screen orientation
+        // changes
+        outState.putParcelable("file_uri", fileUri);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // get the file url
+        fileUri = savedInstanceState.getParcelable("file_uri");
+    }
+
+    /**
+     * Creating file uri to store image/video
+     */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /**
+     * returning image / video
+     */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + ".jpg");
+        }  else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
